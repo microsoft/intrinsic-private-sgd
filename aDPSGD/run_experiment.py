3ab5294e5d01280cf0db3dfea3c5495e4a9de034
Stephanie Hyland <stephanie.hyland@microsoft.com>
2020-04-09 15:31:26 +0000

    experiment identifier generated

--- aDPSGD/run_experiment.py
+++ aDPSGD/run_experiment.py
@@ -8,49 +8,8 @@ from time import time
 
 import ipdb
 
-#import model_utils
-#from data_utils import load_data
-
-def cifar10():
-    init_path = 'models/' + model_type + '/cifar10_binary_init.h5'
-
-def cifar10():
-    init_path = 'models/' + model_type + '/cifar_init.h5'
-
-def cifar10_square():
-    init_path = 'models/' + model_type + '/cifar_square_init.h5'
-
-def mnist_square():
-    init_path = 'models/' + model_type + '/mnist_square_init.h5'
-
-def mnist():
-    init_path = 'models/' + model_type + '/mnist_small_init.h5'
-    init_path = 'models/' + model_type + '/mnist_init.h5'
-
-def mnist_binary():
-    init_path = 'models/' + model_type + '/mnist_binary_init.h5'
-
-def mnist_binary_cropped():
-    init_path = 'models/' + model_type + '/mnist_binary_cropped_init.h5'
-
-def protein():
-    init_path = 'models/' + model_type + '/' + data_type + '_' + model_type + '_init.h5'
-
-def forest():
-    init_path = 'models/' + model_type + '/' + data_type + '_' + model_type + '_init.h5'
-
-def adult():
-    init_path = 'models/' + model_type + '/' + data_type + '_' + model_type + '_init.h5'
-
-def housing():
-    init_path = 'models/' + model_type + '/' + data_type + '_' + model_type + '_init.h5'
-
-def housing_binary():
-    init_path = 'models/' + model_type + '/' + data_type + '_' + model_type + '_init.h5'
-
-def mvn():
-    init_path = 'models/' + model_type + '/' + data_type + '_' + model_type + '_init.h5'
-
+import model_utils
+from data_utils import load_data
 
 def check_cfg_for_consistency(cfg):
     if cfg['data']['binary']:
@@ -71,15 +30,24 @@ def get_model_init_path(cfg):
         init_path = None
     else:
         architecture = cfg['model']['architecture']
-        cfg_name = cfg['name']
+        cfg_name = cfg['cfg_name']
         init_path = os.path.join('models', architecture + '_' + cfg_name + '_init.h5')
     return init_path
 
+def get_experiment_identifier(cfg, seed, replace_index):
+    #identifier = data_type + '/' + data_privacy + '/' + model_type + '/' + model_type + '_DIFFINIT'*diffinit + '.replace_' + str(replace_index) + '.seed_' + str(seed)
+    identifier_pieces = [cfg['cfg_name'],
+            cfg['model']['architecture'],
+            'diffinit'*cfg['model']['diffinit'],
+            'replace' + str(replace_index),
+            'seed' + str(seed)]
+    experiment_identifier = '_'.join(identifier_pieces)
+    return experiment_identifier
 
 def run_experiment(cfg, seed, replace_index):
-    print('Running experiment from cfg:', cfg['cfg_name'])
     t0 = time()
     experiment_identifier = get_experiment_identifier(cfg, seed, replace_index)
+    print('Running experiment with identifier', experiment_identifier)
     # load data
     x_train, y_train, x_vali, y_vali, x_test, y_test = load_data(options=cfg['data'])
     # define model
@@ -98,7 +66,6 @@ def run_experiment(cfg, seed, replace_index):
     print('Finished after', time() - t0, 'seconds')
 
 def main(n_epochs, init_path, seed, replace_index, lr, identifier, data_type, data_privacy, model_type, examine_gradients, cadence, task_type, batch_size):
-    identifier = data_type + '/' + data_privacy + '/' + model_type + '/' + model_type + '_DIFFINIT'*diffinit + '.replace_' + str(replace_index) + '.seed_' + str(seed)
     print('\t\tRunning experiment on identifier:', identifier)
     # loading mnist, ok
     x_train, y_train, x_vali, y_vali, x_test, y_test = load_data(data_type=data_type, replace_index=replace_index, data_privacy=data_privacy)
@@ -115,11 +82,12 @@ def main(n_epochs, init_path, seed, replace_index, lr, identifier, data_type, da
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--cfg', type=str, help='Name of yaml cfg of experiment')
-parser.add_argument('--seed', type=int, help='Random seed used for SGD')
-parser.add_argument('--replace_index', type=int, help='Which training example to replace with x0')
+parser.add_argument('--seed', type=int, help='Random seed used for SGD', default=1)
+parser.add_argument('--replace_index', type=int, help='Which training example to replace with x0', default=None)
 args = parser.parse_args()
 if '.yaml' in args.cfg:
     args.cfg = args.cfg.rstrip('.yaml')
 cfg = yaml.safe_load(open(os.path.join('cfgs', args.cfg + '.yaml')))
 cfg['cfg_name'] = args.cfg
 check_cfg_for_consistency(cfg)
+run_experiment(cfg, args.seed, args.replace_index)
