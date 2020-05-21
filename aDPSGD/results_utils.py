ee03a19c6ecc8c2bf09f11f25452f71b110a246c
Stephanie Hyland <stephanie.hyland@microsoft.com>
2020-05-21 14:18:32 +0000

    initial set of pepping

--- aDPSGD/results_utils.py
+++ aDPSGD/results_utils.py
@@ -1,42 +1,54 @@
 #!/usr/bin/env ipython
-## author: stephanie hyland
-## purpose: Scripts for manipulating experimental results (e.g. mostly loading!)
+# author: stephanie hyland
+# purpose: Scripts for manipulating experimental results (e.g. mostly loading!)
 
 import numpy as np
 import pandas as pd
 import glob
 import os
 
+
 def trace_path_stub(dataset, model, replace_index=None, seed=None, diffinit=False, data_privacy='all'):
     path_stub = './traces/' + dataset + '/' + data_privacy + '/' + model + '/' + model
+
     if diffinit:
         path_stub = path_stub + '_DIFFINIT'
-    if not replace_index is None:
+
+    if replace_index is not None:
         path_stub = path_stub + '.replace_' + str(replace_index)
-    if not seed is None:
+
+    if seed is not None:
         path_stub = path_stub + '.seed_' + str(seed)
+
     return path_stub
+
 
 def get_available_results(dataset, model, replace_index=None, seed=None, diffinit=False, data_privacy='all'):
     # TODO fix this is the worst thing ever
     available_results = ['.'.join(x.split('/')[-1].split('.')[:-2]) for x in glob.glob(trace_path_stub(dataset, model, diffinit=diffinit, data_privacy=data_privacy) + '.*.weights.csv')]
     # remove replace_NA!
-    available_results = [x for x in available_results if not 'replace_NA' in x]
-    # collect all the results 
+    available_results = [x for x in available_results if 'replace_NA' not in x]
+    # collect all the results
     drop_and_replace_and_seeds = [(x.split('.')[1].split('_')[1], x.split('.')[2].split('_')[1], x.split('.')[3].split('_')[1]) for x in available_results]
     drop, replace, seeds = zip(*drop_and_replace_and_seeds)
     df = pd.DataFrame({'drop': drop, 'replace': replace, 'seed': seeds})
-    if not replace_index is None:
+
+    if replace_index is not None:
         df = df.loc[df['replace'] == replace_index, :]
-    if not seed is None:
+
+    if seed is not None:
         df = df.loc[df['seed'] == seed, :]
+
     return df
+
 
 def check_if_experiment_exists(dataset, model, replace_index, seed, diffinit, data_privacy='all'):
     path = trace_path_stub(dataset, model, replace_index, seed, diffinit, data_privacy) + '.weights.csv'
     exists = os.path.exists(path)
+
     if not exists:
         logpath = 'missing_experiments.' + dataset + '.' + data_privacy + '.' + model + '.csv'
+
         if not os.path.exists(logpath):
             logfile = open(logpath, 'w')
             logfile.write('replace,seed,diffinit\n')
@@ -44,26 +56,37 @@ def check_if_experiment_exists(dataset, model, replace_index, seed, diffinit, da
             logfile = open(logpath, 'a')
         logfile.write(str(replace_index) + ',' + str(seed) + ',' + str(diffinit) + '\n')
         logfile.close()
+
     return exists
 
-def get_posterior_samples(dataset, iter_range, model='linear', replace_index=None, params=None, seeds='all', n_seeds=None, verbose=True, diffinit=False, data_privacy='all'):
+
+def get_posterior_samples(dataset, iter_range, model='linear', replace_index=None,
+                          params=None, seeds='all', n_seeds=None, verbose=True,
+                          diffinit=False, data_privacy='all'):
     """
     grab the values of the weights of [params] at [at_time] for all the available seeds from identifier_stub
     might want to re-integrate this with sacred at some point
     """
+
     if seeds == 'all':
-        df = get_available_results(dataset, model, replace_index=replace_index, diffinit=diffinit, data_privacy=data_privacy)
+        df = get_available_results(dataset, model, replace_index=replace_index,
+                                   diffinit=diffinit, data_privacy=data_privacy)
         available_seeds = df['seed'].unique().tolist()
     else:
         assert type(seeds) == list
         available_seeds = seeds
-    if not n_seeds is None:
+
+    if n_seeds is not None:
         available_seeds = np.random.choice(available_seeds, n_seeds, replace=False)
+
     if verbose:
         print('Loading samples from seeds:', available_seeds, 'in range', iter_range)
     samples = []
+
     for i, s in enumerate(available_seeds):
-        weights_from_s = load_weights(dataset, model, replace_index=replace_index, seed=s, iter_range=iter_range, params=params, verbose=False, diffinit=diffinit, data_privacy=data_privacy)
+        weights_from_s = load_weights(dataset, model, replace_index=replace_index,
+                                      seed=s, iter_range=iter_range, params=params,
+                                      verbose=False, diffinit=diffinit, data_privacy=data_privacy)
         try:
             if weights_from_s.shape[0] == 0:
                 print('WARNING: No data from seed', s, 'in range', iter_range, ' - skipping')
@@ -73,19 +96,25 @@ def get_posterior_samples(dataset, iter_range, model='linear', replace_index=Non
                 samples.append(weights_from_s)
         except AttributeError:
             print('WARNING: No data from seed', s, 'in range', iter_range, 'or something, not sure why this error happened? - skipping')
+
     if len(samples) > 1:
         samples = pd.concat(samples)
     else:
         print('[get_posterior_samples] WARNING: No actual samples acquired for replace', replace_index, '!')
         samples = False
+
     return samples
 
-def load_gradients(dataset, model, replace_index, seed, noise=False, iter_range=(None, None), params=None, verbose=False, diffinit=False):
+
+def load_gradients(dataset, model, replace_index, seed, noise=False,
+                   iter_range=(None, None), params=None, verbose=False, diffinit=False):
     path_stub = trace_path_stub(dataset, model, replace_index, seed, diffinit=diffinit)
-    path = path_stub + '.all_gradients.csv' 
-    if not params is None:
+    path = path_stub + '.all_gradients.csv'
+
+    if params is not None:
         assert type(params) == list
         usecols = ['t', 'minibatch_id'] + params
+
         if verbose:
             print('Loading gradients with columns:', usecols)
     else:
@@ -93,18 +122,20 @@ def load_gradients(dataset, model, replace_index, seed, noise=False, iter_range=
             print('WARNING: Loading all columns can be slow!')
         usecols = None
     df = pd.read_csv(path, skiprows=1, usecols=usecols, dtype={'t': np.int64, 'minibatch_id': str})
-    
+
     # remove validation data by default
     df = df.loc[~(df['minibatch_id'] == 'VALI'), :]
-    
+
     if iter_range[0] is not None:
         df = df.loc[df['t'] >= iter_range[0], :]
+
     if iter_range[1] is not None:
         df = df.loc[df['t'] <= iter_range[1], :]
-    
+
     if noise:
         # separate minibatches from aggregate
         df_minibatch = df.loc[~(df['minibatch_id'] == 'ALL'), :]
+
         if df_minibatch.shape[0] == 0:
             print('[load_gradients] WARNING: No minibatch information. Try turning off calculation of gradient noise')
         df_all = df.loc[df['minibatch_id'] == 'ALL', :]
@@ -112,36 +143,48 @@ def load_gradients(dataset, model, replace_index, seed, noise=False, iter_range=
         df_all = df_all.set_index('t').drop('minibatch_id', axis=1)
         df = df_minibatch - df_all
         df.reset_index(inplace=True)
+
     return df
 
-def load_weights(dataset, model, replace_index, seed, diffinit=False, 
-        iter_range=(None, None), params=None, verbose=True, data_privacy='all'):
+
+def load_weights(dataset, model, replace_index, seed, diffinit=False,
+                 iter_range=(None, None), params=None, verbose=True, data_privacy='all'):
     path_stub = trace_path_stub(dataset, model, replace_index, seed, diffinit=diffinit, data_privacy=data_privacy)
-    path = path_stub + '.weights.csv' 
-    if not params is None:
+    path = path_stub + '.weights.csv'
+
+    if params is not None:
         assert type(params) == list
         usecols = ['t'] + params
     else:
-        if verbose: print('WARNING: Loading all columns can be slow!')
+        if verbose:
+            print('WARNING: Loading all columns can be slow!')
         usecols = None
-    
+
     df = pd.read_csv(path, skiprows=1, usecols=usecols)
-    
+
     if iter_range[0] is not None:
         df = df.loc[df['t'] >= iter_range[0], :]
+
     if iter_range[1] is not None:
         df = df.loc[df['t'] <= iter_range[1], :]
-    
-    if verbose: print('Loaded weights from', path)
+
+    if verbose:
+        print('Loaded weights from', path)
+
     return df
 
-def load_loss(dataset, model, replace_index, seed, iter_range=(None, None), diffinit=False, verbose=False, data_privacy='all'):
-    path_stub = trace_path_stub(dataset, model, replace_index, seed, diffinit=diffinit, data_privacy=data_privacy)
-    path = path_stub + '.loss.csv' 
+
+def load_loss(dataset, model, replace_index, seed, iter_range=(None, None),
+              diffinit=False, verbose=False, data_privacy='all'):
+    path_stub = trace_path_stub(dataset, model, replace_index, seed, diffinit=diffinit,
+                                data_privacy=data_privacy)
+    path = path_stub + '.loss.csv'
     df = pd.read_csv(path, skiprows=1)
-    
+
     if iter_range[0] is not None:
         df = df.loc[df['t'] >= iter_range[0], :]
+
     if iter_range[1] is not None:
         df = df.loc[df['t'] <= iter_range[1], :]
+
     return df
--- aDPSGD/run_experiment.py
+++ aDPSGD/run_experiment.py
@@ -12,19 +12,23 @@ import ipdb
 import model_utils
 from data_utils import load_data
 
+
 def check_cfg_for_consistency(cfg):
     if cfg['data']['binary']:
         assert cfg['model']['task_type'] == 'binary'
         assert cfg['model']['output_size'] == 1
+
     if 'flatten' in cfg['data']:
         if cfg['data']['flatten'] is True:
             assert type(cfg['model']['input_size']) == int
         else:
             assert len(cfg['model']['input_size']) > 1
+
     if cfg['model']['architecture'] in ['logistic', 'linear']:
         if cfg['model']['hidden_size'] is not None:
             print('WARNING: Hidden size specified for logistic or linear model!')
     print('cfg passed checks')
+
 
 def get_model_init_path(cfg):
     if cfg['model']['diffinit'] is True:
@@ -32,17 +36,22 @@ def get_model_init_path(cfg):
     else:
         architecture = cfg['model']['architecture']
         cfg_name = cfg['cfg_name']
-        init_path = os.path.join('models', architecture + '_' + cfg_name + '_init.h5')
+        init_path = f'{architecture}_{cfg_name}_init.h5'
+        init_path = Path('models') / init_path
+
     return init_path
+
 
 def get_experiment_identifier(cfg, seed, replace_index):
     identifier_pieces = [cfg['cfg_name'],
-            cfg['model']['architecture'],
-            'diffinit'*cfg['model']['diffinit'],
-            'replace' + str(replace_index),
-            'seed' + str(seed)]
+                         cfg['model']['architecture'],
+                         'diffinit'*cfg['model']['diffinit'],
+                         'replace' + str(replace_index),
+                         'seed' + str(seed)]
     experiment_identifier = '_'.join(identifier_pieces)
+
     return experiment_identifier
+
 
 def run_experiment(cfg, seed, replace_index):
     t0 = time()
@@ -55,24 +64,26 @@ def run_experiment(cfg, seed, replace_index):
     model = model_utils.build_model(**cfg['model'], init_path=init_path)
     # prep model for training
     model_utils.prep_for_training(model, seed=seed,
-            optimizer_settings=cfg['training']['optimization_algorithm'],
-            task_type=cfg['model']['task_type'])
+                                  optimizer_settings=cfg['training']['optimization_algorithm'],
+                                  task_type=cfg['model']['task_type'])
     # now train
-    model_utils.train_model(model, cfg['training'], cfg['logging'], 
-            x_train, y_train, x_vali, y_vali,
-            experiment_identifier=experiment_identifier)
+    model_utils.train_model(model, cfg['training'], cfg['logging'],
+                            x_train, y_train, x_vali, y_vali,
+                            experiment_identifier=experiment_identifier)
     # clean up
     del model
     print('Finished after', time() - t0, 'seconds')
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--cfg', type=str, help='Name of yaml cfg of experiment')
 parser.add_argument('--seed', type=int, help='Random seed used for SGD', default=1)
 parser.add_argument('--replace_index', type=int, help='Which training example to replace with x0', default=None)
 args = parser.parse_args()
+
 if '.yaml' in args.cfg:
     args.cfg = args.cfg.rstrip('.yaml')
-#cfg = yaml.safe_load(open(os.path.join('cfgs', args.cfg + '.yaml')))
+# cfg = yaml.safe_load(open(os.path.join('cfgs', args.cfg + '.yaml')))
 cfg = yaml.load(open(os.path.join('cfgs', args.cfg + '.yaml')))
 cfg['cfg_name'] = args.cfg
 check_cfg_for_consistency(cfg)
